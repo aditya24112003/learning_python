80cee87e07073159ba3364f090a2353cc057ce59
aditya <srivastavadi045@gmail.com>
2022-06-13 10:08:27 +0530

    learning if else

--- dict_4.py
+++ dict_4.py
@@ -1,2 +1,26 @@
 d = {"aditya": 20}
 print(d)
+a = {'key' :'value', 'cow' :'mooh'}
+print(a['cow'])
+# Dictionary is nothing but key value pairs
+d1 = {}
+# print(type(d1))
+d2 = {"Aditya":"Burger",
+      "Rohan":"Fish",
+      "SkillF":"Roti",
+      "Shubham":{"B":"maggie", "L":"roti", "D":"Chicken"}}
+print(d2)
+
+print(d2["Shubham"])
+d3 = d2.copy()
+del d3["Aditya"]
+d2.update({"Leena":"Toffee"})
+print(d2.keys())
+print(d2.items())
+
+
+d4 = {"Mutable": "Can change", "Imuatable": "Cannot change",
+      "Thesis" : "Theory", "Fear":"Terror"}
+print("Enter your word")
+key = input()
+print(d4[key])
---
+++ sets_5.py
@@ -0,0 +1,4 @@
+s = set()
+s_from_list = set([1,2,3,4,5])
+print(s_from_list)
+print(type(s_from_list))
