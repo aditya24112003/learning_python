b7067e521f2598427da839f3f32441f28cf5ee27
aditya <srivastavadi045@gmail.com>
2022-06-13 13:47:21 +0530

    learning loops

---
+++ loops_7.py
@@ -0,0 +1,23 @@
+"""total = 0
+for i in range(1,11):
+    total = total + i
+print(total)"""
+"""total = 0
+num = int(input("enter a number" ))
+for i in range(1,num+1):
+    total = total + i
+print (total)"""
+"""sum = 0
+n = input("enter a number")
+for i in range(0,len(n)):
+    sum = sum +int(n[i])
+print(sum)"""
+"""number = int(input(" Please enter any Number : "))
+fact = 1
+
+for i in range(1, number + 1):
+    fact = fact * i
+print(fact)"""
+n = input("enter a string")
+for i in range(len(n)-1 ,-1,-1):
+    print(n[i],end="")
