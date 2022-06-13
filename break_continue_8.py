7f292c5de7d215eaacaf5528f1f887e51c26401a
aditya <srivastavadi045@gmail.com>
2022-06-13 22:42:19 +0530

    learning

---
+++ break_continue_8.py
@@ -0,0 +1,16 @@
+"""i = 0
+while i<45:
+    print(i)
+    if i ==22:
+        break
+    i=i+1"""
+"""for i in range(1,101):
+    if i%3==0:
+        break
+    print(i)"""
+for i in range(1,101):
+    if i%3==0:
+        continue
+    print(i)
+
+
