7deb3fd5884baa09504ed5ed6650adafd595e953
aditya <srivastavadi045@gmail.com>
2022-06-13 17:48:55 +0530

    learning loops

--- loops_7.py
+++ loops_7.py
@@ -18,10 +18,38 @@ fact = 1
 for i in range(1, number + 1):
     fact = fact * i
 print(fact)"""
-n = input("enter a string")
+"""n = input("enter a string")
 for i in range(len(n)-1 ,-1,-1):
-    print(n[i],end="")
-n = int(input("enter a number"))
+    print(n[i],end="")"""
+"""n = int(input("enter a number"))
 total = 0
-for i in range(1,n):
+for i in range(1,n):"""
+
+lst1 = [111, 32, -9, -45, -17, 9, 85, -10]
+lst2 = []
+for i in lst1:
+    if i > 0:
+        lst2.append(i)
+print(lst2)
+
+lst3=[3, 7, 6, 8, 9, 11, 15, 25]
+lst4=[]
+for i in lst3:
+    lst4.append(i**2)
+
+
+
+print(lst4)
+
+
+
+dict={"z1":900, "t1": 1100, "p1": 2300, "r1": 1050, "k1": 3200, "g1": 400}
+lst=[]
+for i in dict:
+    if dict[i] > 1000:
+        lst.append(dict[i]-1000)
+
+print(lst)
+
+
 
